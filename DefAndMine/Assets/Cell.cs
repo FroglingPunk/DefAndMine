1d31318389770e569cb69bc832c9efba698ba53e
Rishat Muhamedov <alltimew@mail.ru>
2020-07-15 16:28:26 +0300

    Init version

---
+++ DefAndMine/Assets/Cell.cs
@@ -0,0 +1,188 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Cell : MonoBehaviour
+{
+    public Field Field;
+    public int IDx;
+    public int IDz;
+
+    private bool _hasPower;
+    private bool _hasPower_isDirty;
+
+    private Cell[] neighbors;
+
+    public bool HasPower
+    {
+        get
+        {
+            return _hasPower;
+        }
+        set
+        {
+            if (!_hasPower_isDirty)
+            {
+                _hasPower_isDirty = true;
+                StartCoroutine(HasPowerValueCleaner());
+
+                _hasPower = value;
+
+                for (EDirection direction = EDirection.N; direction <= EDirection.W; direction++)
+                {
+                    if (HasNeighbor(direction))
+                    {
+                        GetNeighbor(direction).OnNeighborCellPowerChanged(this);
+                    }
+                }
+
+                RebuildPowerConductors();
+            }
+        }
+    }
+
+
+    [SerializeField]
+    private Transform powerX_trans;
+    [SerializeField]
+    private Transform powerZ_trans;
+
+    private void RebuildPowerConductors()
+    {
+        if (_hasPower)
+        {
+            bool[] neighborsHasPower =
+            {
+                        NeighborHasPower(EDirection.N),
+                        NeighborHasPower(EDirection.E),
+                        NeighborHasPower(EDirection.S),
+                        NeighborHasPower(EDirection.W)
+                    };
+
+            if (neighborsHasPower[(int)EDirection.E] && neighborsHasPower[(int)EDirection.W])
+            {
+                powerX_trans.localScale = new Vector3(1f, 0.1f, 0.1f);
+                powerX_trans.localPosition = Vector3.zero;
+            }
+            else if (neighborsHasPower[(int)EDirection.E])
+            {
+                powerX_trans.localScale = new Vector3(0.55f, 0.1f, 0.1f);
+                powerX_trans.localPosition = new Vector3(0.225f, 0f, 0f);
+            }
+            else if (neighborsHasPower[(int)EDirection.W])
+            {
+                powerX_trans.localScale = new Vector3(0.55f, 0.1f, 0.1f);
+                powerX_trans.localPosition = new Vector3(-0.225f, 0f, 0f);
+            }
+            else
+            {
+                powerX_trans.localScale = new Vector3(0.3f, 0.1f, 0.3f);
+                powerX_trans.localPosition = Vector3.zero;
+            }
+
+            if (neighborsHasPower[(int)EDirection.N] && neighborsHasPower[(int)EDirection.S])
+            {
+                powerZ_trans.localScale = new Vector3(0.1f, 0.1f, 1f);
+                powerZ_trans.localPosition = Vector3.zero;
+            }
+            else if (neighborsHasPower[(int)EDirection.N])
+            {
+                powerZ_trans.localScale = new Vector3(0.1f, 0.1f, 0.55f);
+                powerZ_trans.localPosition = new Vector3(0f, 0f, 0.225f);
+            }
+            else if (neighborsHasPower[(int)EDirection.S])
+            {
+                powerZ_trans.localScale = new Vector3(0.1f, 0.1f, 0.55f);
+                powerZ_trans.localPosition = new Vector3(0f, 0f, -0.225f);
+            }
+            else
+            {
+                powerZ_trans.localScale = new Vector3(0.3f, 0.1f, 0.3f);
+                powerZ_trans.localPosition = Vector3.zero;
+            }
+        }
+        else
+        {
+            powerX_trans.localScale = Vector3.zero;
+            powerZ_trans.localScale = Vector3.zero;
+        }
+    }
+
+    IEnumerator HasPowerValueCleaner()
+    {
+        yield return null;
+        _hasPower_isDirty = false;
+    }
+
+    public void Init(Field field, int id_x, int id_z)
+    {
+        Field = field;
+        IDx = id_x;
+        IDz = id_z;
+
+        neighbors = new Cell[4];
+    }
+
+    public void SwitchPower()
+    {
+        HasPower = !HasPower;
+    }
+
+    public void ChainDisablePower()
+    {
+        if (!_hasPower_isDirty && HasPower)
+        {
+            HasPower = false;
+
+            for (EDirection direction = EDirection.N; direction <= EDirection.W; direction++)
+            {
+                if (HasNeighbor(direction))
+                {
+                    GetNeighbor(direction).ChainDisablePower();
+                }
+            }
+        }
+    }
+
+    private void OnNeighborCellPowerChanged(Cell cell)
+    {
+        RebuildPowerConductors();
+    }
+
+    public bool HasNeighbor(EDirection direction)
+    {
+        return neighbors[(int)direction] != null;
+    }
+
+    public Cell GetNeighbor(EDirection direction)
+    {
+        return neighbors[(int)direction];
+    }
+
+    public void SetNeighbor(Cell neighbor, EDirection direction)
+    {
+        neighbors[(int)direction] = neighbor;
+        neighbor.neighbors[(int)direction.Opposite()] = this;
+    }
+
+    public bool NeighborHasPower(EDirection direction)
+    {
+        Cell neighbor = GetNeighbor(direction);
+        return neighbor != null && neighbor.HasPower;
+    }
+}
+
+public enum EDirection
+{
+    N,
+    E,
+    S,
+    W
+}
+
+static public class EDirectionExtensions
+{
+    static public EDirection Opposite(this EDirection direction)
+    {
+        return direction < EDirection.S ? direction + 2 : direction - 2;
+    }
+}
---
+++ DefAndMine/Assets/Field.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Field : MonoBehaviour
+{
+    [SerializeField]
+    private Cell cell_prefab;
+    [SerializeField]
+    private Transform cells_parent;
+
+    [SerializeField]
+    private int width;
+    [SerializeField]
+    private int length;
+
+    private Cell[,] cells;
+
+    public Cell this[int x, int z] => cells[x, z];
+
+    public int Width => width;
+    public int Length => length;
+
+
+
+    void Start()
+    {
+        cells = new Cell[Width, Length];
+
+        for (int z = 0; z < Length; z++)
+        {
+            for (int x = 0; x < Width; x++)
+            {
+                cells[x, z] = Instantiate(cell_prefab, new Vector3(-Width / 2f + x + 0.5f, 0, -Length / 2f + z + 0.5f), Quaternion.identity, cells_parent);
+                cells[x, z].Init(this, x, z);
+
+                if (x > 0)
+                {
+                    cells[x, z].SetNeighbor(cells[x - 1, z], EDirection.W);
+                }
+
+                if (z > 0)
+                {
+                    cells[x, z].SetNeighbor(cells[x, z - 1], EDirection.S);
+                }
+
+                cells[x, z].HasPower = false;
+            }
+        }
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if(Physics.Raycast(ray,out hit))
+            {
+                Cell cell;
+                if (hit.collider.TryGetComponent(out cell))
+                {
+                    cell.SwitchPower();
+                }
+            }
+        }
+
+        if (Input.GetMouseButtonDown(1))
+        {
+            RaycastHit hit;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Cell cell;
+                if (hit.collider.TryGetComponent(out cell))
+                {
+                    cell.ChainDisablePower();
+                }
+            }
+        }
+    }
+}
